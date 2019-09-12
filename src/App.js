a9af6922a70ca5d456fcba48c81bcfb13997d628
Angie Zhou <angelazhou.23@gmail.com>
2019-09-12 13:03:27 -0700

    react intro app

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import Person from './Person/Person';
+import './App.css';
+
+class App extends Component {
+    state = {
+        persons: [
+            { name: 'Cool', age: 29},
+            { name: 'Beans', age: 30 },
+            { name: 'Bodie', age: 10 }
+
+        ],
+
+        otherState: 'some value'
+
+
+    }
+
+    switchName = () => {
+        //console.log('was clicked');
+        this.setState({
+            persons: [
+                { name: 'Bakery', age: 29 },
+                { name: 'Beans', age: 30 },
+                { name: 'Bodie', age: 13 }
+            ]
+        })
+    }
+
+      render() {
+        return (
+          <div className="App">
+            <h1>Welcome to My Page</h1>
+                <button onClick={this.switchName}>Switch</button>
+                <Person name={this.state.persons[0].name} age={this.state.persons[0].age}  />
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+                <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+          </div>
+          );
+
+          //return React.createElement('div', {className: 'App' }, null, React.createElement('h1', null, 'Hello there'));
+      }
+}
+
+export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+const person = (props) => {
+    return <p>hello human, I'm {props.name} and I'm {props.age}</p>
+};
+
+export default person;
